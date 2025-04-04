a5f2ce0847eff9df1de3f4b78f9bad559a07ad09
Nam Khanh <namkhanh090703@gmail.com>
2025-04-04 23:21:20 +0700

    Update ShowTest.java

--- ShowTest.java
+++ ShowTest.java
@@ -4,10 +4,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import cinema.ticket.booking.repository.*;
+import cinema.ticket.booking.exception.MyBadRequestException;
 import cinema.ticket.booking.exception.MyNotFoundException;
 import cinema.ticket.booking.model.*;
 import cinema.ticket.booking.request.ShowRequest;
+import cinema.ticket.booking.response.ErrorResponse;
 import cinema.ticket.booking.response.MyApiResponse;
 import cinema.ticket.booking.response.ShowInfoResponse;
 import cinema.ticket.booking.service.CinemaHallService;
@@ -34,11 +37,12 @@ class ShowTest {
     private CinemaHall testHall;
     private Movie movie1;
     private CinemaShow testShow;
+    @Autowired
     private CinemaHallService cinemaHallService;
 
     @BeforeEach
     public void setUp() {
-        testHall = cinemaHallService.getHallById("E");
+        testHall = cinemaHallService.getHallById("bd52169a-73795f48-9625ea04");
 
         movie1 =new Movie();
         movie1.settitle("Avengers");
@@ -51,8 +55,8 @@ class ShowTest {
         movie1.setActors("People");
         movieRepository.save(movie1);
 
-        LocalDateTime startTime = LocalDateTime.of(2025, 5, 1, 18, 0);
-        String startTime1=startTime.toString();
+        //LocalDateTime startTime = LocalDateTime.of(2025, 5, 1, 18, 0);
+        String startTime1="01/05/2025 18:00";
         testShow = new CinemaShow(testHall, movie1, startTime1);
         showRepository.save(testShow);
     }
@@ -63,15 +67,15 @@ class ShowTest {
 
         Field cinemaID = showRequest.getClass().getDeclaredField("cinemaID");
         cinemaID.setAccessible(true);
-        cinemaID.set(showRequest, 1L);
+        cinemaID.set(showRequest,testHall.getId());
 
         Field movieID = showRequest.getClass().getDeclaredField("movieID");
         movieID.setAccessible(true);
-        movieID.set(showRequest, 1L);
+        movieID.set(showRequest, movie1.getId());
 
-        Field startTime = showRequest.getClass().getDeclaredField("startTime");
+        Field startTime = showRequest.getClass().getDeclaredField("start_time");
         startTime.setAccessible(true);
-        String startTime2="2025-05-03T18:00:00";
+        String startTime2="03/05/2025 12:00";
         startTime.set(showRequest, startTime2);
 
         MyApiResponse response = cinemaShowService.addShow(showRequest);
@@ -85,15 +89,15 @@ class ShowTest {
 
         Field cinemaID = showRequest.getClass().getDeclaredField("cinemaID");
         cinemaID.setAccessible(true);
-        cinemaID.set(showRequest, 2L);
+        cinemaID.set(showRequest, testHall.getId());
 
         Field movieID = showRequest.getClass().getDeclaredField("movieID");
         movieID.setAccessible(true);
-        movieID.set(showRequest, 2L);
+        movieID.set(showRequest,movie1.getId());
 
-        MyApiResponse response = cinemaShowService.addShow(showRequest);
-        assertNotNull(response);
-        assertTrue(response.getMessage().length() > 0);
+        Exception exception = assertThrows(MyBadRequestException.class, 
+        () -> cinemaShowService.addShow(showRequest));
+        assertEquals(exception.getMessage(),"Invaild date format, it must be dd/MM/yyyy HH:mm");
     }
 //Test get show info by ID
     @Test
@@ -106,6 +110,7 @@ class ShowTest {
         assertTrue(showInfoResponse.getTotalReversedSeats() >= 0);
     }
 //Test get show info by invalid ID
+    @Test
     void testGetShowInfo_InvalidID() {
         Exception exception = assertThrows(MyNotFoundException.class, 
         () -> cinemaShowService.getShowInfo("9999"));
@@ -126,15 +131,15 @@ class ShowTest {
 
         Field cinemaID = updateRequest.getClass().getDeclaredField("cinemaID");
         cinemaID.setAccessible(true);
-        cinemaID.set(updateRequest, 1L);
+        cinemaID.set(updateRequest, testHall.getId());
 
         Field movieID = updateRequest.getClass().getDeclaredField("movieID");
         movieID.setAccessible(true);
-        movieID.set(updateRequest, 1L);
+        movieID.set(updateRequest, movie1.getId());
 
-        Field startTime = updateRequest.getClass().getDeclaredField("startTime");
+        Field startTime = updateRequest.getClass().getDeclaredField("start_time");
         startTime.setAccessible(true);
-        String startTime2="2025-05-02T18:00:00";
+        String startTime2="03/05/2025 12:00";
         startTime.set(updateRequest, startTime2);
         
         MyApiResponse response = cinemaShowService.updateShow(testShow.getId(), updateRequest);
@@ -143,7 +148,8 @@ class ShowTest {
         assertEquals("Done", response.getMessage());
 
         CinemaShow updatedShow = showRepository.findById(testShow.getId()).orElseThrow();
-        assertEquals(LocalDateTime.of(2025, 5, 2, 18, 0), updatedShow.getStartTime());
+        assertEquals(LocalDateTime.of(2025, 5, 3, 12, 0), updatedShow.getStartTime());
+        //if (starttime != null && starttime.equals(show.getStartTime()))
     }
 //Update show with null time
     @Test
@@ -152,11 +158,11 @@ class ShowTest {
 
         Field cinemaID = updateRequest.getClass().getDeclaredField("cinemaID");
         cinemaID.setAccessible(true);
-        cinemaID.set(updateRequest, 1L);
+        cinemaID.set(updateRequest, testHall.getId());
 
         Field movieID = updateRequest.getClass().getDeclaredField("movieID");
         movieID.setAccessible(true);
-        movieID.set(updateRequest, 1L);
+        movieID.set(updateRequest, movie1.getId());
         
         MyApiResponse response = cinemaShowService.updateShow(testShow.getId(), updateRequest);
         assertNotNull(response);
@@ -165,22 +171,19 @@ class ShowTest {
         CinemaShow updatedShow = showRepository.findById(testShow.getId()).orElseThrow();
         assertEquals(testShow.getStartTime(), updatedShow.getStartTime());
     }
+
 // Update show with invalid ID
     @Test
     void testUpdateShow_InvalidID() throws Exception {
         ShowRequest updateRequest = new ShowRequest();
-
         Field cinemaID = updateRequest.getClass().getDeclaredField("cinemaID");
         cinemaID.setAccessible(true);
-        cinemaID.set(updateRequest, 1L);
-
+        cinemaID.set(updateRequest, testHall.getId());
         Field movieID = updateRequest.getClass().getDeclaredField("movieID");
         movieID.setAccessible(true);
-        movieID.set(updateRequest, 1L);
-
+        movieID.set(updateRequest, movie1.getId());
         Exception exception = assertThrows(MyNotFoundException.class, 
         () -> cinemaShowService.updateShow("9999", updateRequest));
-
         assertEquals("Show is not found", exception.getMessage());
     }
 //Delete show by ID
@@ -193,10 +196,11 @@ class ShowTest {
 //Delete show by invalid ID
     @Test
     void testDeleteShow_InvalidID() {
-        Exception exception = assertThrows(MyNotFoundException.class, 
-        () -> cinemaShowService.deleteShow("9999"));
-
-        assertEquals("Show is not found", exception.getMessage());
+        MyApiResponse response = cinemaShowService.deleteShow("9999");
+        assertNotNull(response);
+        assertTrue(response instanceof ErrorResponse);
+        assertEquals("Show is not found", response.getMessage());
+        //code message is "Show is found"
     }
 //Delete show by hall ID and movie ID
     @Test
@@ -205,15 +209,15 @@ class ShowTest {
 
         Field cinemaID = showRequest.getClass().getDeclaredField("cinemaID");
         cinemaID.setAccessible(true);
-        cinemaID.set(showRequest, 1L);
+        cinemaID.set(showRequest, testHall.getId());
 
         Field movieID = showRequest.getClass().getDeclaredField("movieID");
         movieID.setAccessible(true);
-        movieID.set(showRequest, 1L);
+        movieID.set(showRequest, movie1.getId());
 
-        Field startTime = showRequest.getClass().getDeclaredField("startTime");
+        Field startTime = showRequest.getClass().getDeclaredField("start_time");
         startTime.setAccessible(true);
-        String startTime2="2025-05-02T18:00:00";
+        String startTime2="01/05/2025 18:00";
         startTime.set(showRequest, startTime2);
 
         MyApiResponse response = cinemaShowService.deleteShowByHallIDMovieID(showRequest);
@@ -227,13 +231,13 @@ class ShowTest {
 
         Field cinemaID = showRequest.getClass().getDeclaredField("cinemaID");
         cinemaID.setAccessible(true);
-        cinemaID.set(showRequest, 1L);
+        cinemaID.set(showRequest,testHall.getId());
 
         Field movieID = showRequest.getClass().getDeclaredField("movieID");
         movieID.setAccessible(true);
-        movieID.set(showRequest, 1L);
+        movieID.set(showRequest, movie1.getId());
 
-        Exception e=assertThrows(MyNotFoundException.class, 
+        Exception e=assertThrows(MyBadRequestException.class, 
         () -> cinemaShowService.deleteShowByHallIDMovieID(showRequest));
 
         assertEquals("Invaild date format, it must be dd/MM/yyyy HH:mm", e.getMessage());
