428ddf0074a7beb1049d675572d77e85814c3dd2
Nam Khanh <namkhanh090703@gmail.com>
2025-04-04 14:53:08 +0700

    Update ShowTest.java

--- ShowTest.java
+++ ShowTest.java
@@ -19,7 +19,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-class CinemaShowServiceImplTest {
+class ShowTest {
 
     @Autowired
     private CinemaShowServiceImpl cinemaShowService;
